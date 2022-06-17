78f7455e7c2d2973e68e017671d20c794a67aa9e
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-17 13:25:12 +0700

    import auth routes middleware

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
+import authRoutes from './routes/auth.js';
+
 app.use(bodyParser.json());
+
+app.use(authRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log('app listening');
