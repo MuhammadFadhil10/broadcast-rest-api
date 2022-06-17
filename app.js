2d654953510dfac5fc0801c7e059b0b9a997bb35
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-17 13:07:25 +0700

    add model view router folder

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/', (req, res, next) => {
-	return res.json({ message: 'halo brodi! :D' });
-});
 app.listen(process.env.PORT || 5000, () => {
 	console.log('app listening');
 });
